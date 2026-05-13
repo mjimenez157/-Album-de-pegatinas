3d5f9f94377b09cc8d8693df4322787a5ce6b54b
mjimenez157 <mjimenez157@unab.edu.co>
2026-05-12 22:13:55 -0500

    Create Grupo.java

---
+++ Grupo.java
@@ -0,0 +1,19 @@
+package paq;
+
+public class Grupo {
+
+    private String nombre;
+
+    public Grupo(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
